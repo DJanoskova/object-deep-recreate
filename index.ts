12a2bcb71a1d74de4a275a93924264ee52b73e5f
Dana Janoskova <dana@moonpay.com>
2022-02-14 22:03:44 +0100

    first version

---
+++ index.ts
@@ -0,0 +1,52 @@
+/**
+ * Make sure the compared values are of the same type
+ * @param newValue - altered in-memory data
+ * @param prevValue - cloned snapshot of previous data
+ */
+export function appendNewValues<T = unknown>(newValue: T, prevValue: T) {
+  // for primitive types return right away
+  if (typeof newValue !== 'object') return newValue;
+
+  if (Array.isArray(newValue)) {
+    if (!Array.isArray(prevValue)) {
+      throw new Error('Comparing array with non-array');
+    }
+
+    let result: any[];
+
+    // check every element if same length
+    if (newValue.length === prevValue.length) {
+      result = []
+
+      newValue.forEach((v, index) => {
+        const oldV = prevValue[index];
+        result.push(appendNewValues(v, oldV))
+      })
+    } else {
+      // else removing or adding to array, just create a new instance
+      result = [...newValue]
+    }
+
+    return result;
+  }
+
+  return appendNewObjectValues(newValue, prevValue);
+}
+
+/**
+ * @param newObject
+ * @param prevObject - cloned snapshot of previous data
+ */
+export function appendNewObjectValues<T = Record<any, unknown>>(newObject: T, prevObject: T) {
+  if (JSON.stringify(newObject) === JSON.stringify(prevObject)) return newObject;
+
+  const result: Record<any, unknown> = {}
+
+  Object.keys(newObject).forEach((key: any) => {
+    const newValue = (newObject as any)?.[key];
+    const prevValue = (prevObject as any)?.[key];
+    result[key] = appendNewValues(newValue, prevValue);
+  })
+
+  return result
+}
